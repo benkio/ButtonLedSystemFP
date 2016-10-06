dc8de76243e749de6e77687e68a3826e90797cd1
benkio <benkio89@gmail.com>
2016-10-06 13:34:08 +0200

    Add the test for the Log

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,18 @@
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
-import DomainModel
+import DomainModel.Core
 import Numeric.Natural
+import Data.Functor.Identity
 
 main :: IO ()
 main = hspec $ do
   describe "Switch function" $ do
     it "return the input after even calls, the inverse otherwise" $ property $
       switchProperty
+  describe "log Function" $ do
+    it "return the input plus the log message" $ property $ do
+      logProperty
 
 applyNTimes :: (Enum b, Num b) => b -> (a -> a) -> a -> a
 applyNTimes n f val = foldl (\s e -> e s) val [f | x <- [1..n]]
@@ -18,3 +22,6 @@ switchProperty x y = if (y `mod` 2 == 0)
                      then getLedStatus (applyNTimes y switch l) == (getLedStatus l)
                      else getLedStatus (applyNTimes y switch l) == not (getLedStatus l)
                           where l = Led{status=x}
+
+logProperty :: Int -> String -> Bool
+logProperty a b = runIdentity $ logBLS a b >>= \(l,label) -> return $ label == ("Log message: " ++ b ++ "\n") && l == a
